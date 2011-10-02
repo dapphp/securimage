fc14e5cb702e80c04ad1725c3d0221c51d8d0dde
Drew Phillips <drew@drew-phillips.com>
2011-10-01 21:09:35 -0700

    Update examples for version 3.0

--- securimage_show_example2.php
+++ securimage_show_example2.php
@@ -4,19 +4,29 @@
  * Project:     Securimage: A PHP class for creating and managing form CAPTCHA images<br />
  * File:        securimage_show_example2.php<br />
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or any later version.<br /><br />
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.<br /><br />
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA<br /><br />
+ * Copyright (c) 2011, Drew Phillips
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  *
  * Any modifications to the library should be indicated clearly in the source code
  * to inform users that the changes are not a part of the original software.<br /><br />
@@ -27,29 +37,27 @@
  * @link http://www.phpcaptcha.org Securimage PHP CAPTCHA
  * @link http://www.phpcaptcha.org/latest.zip Download Latest Version
  * @link http://www.phpcaptcha.org/Securimage_Docs/ Online Documentation
- * @copyright 2009 Drew Phillips
+ * @copyright 2011 Drew Phillips
  * @author Drew Phillips <drew@drew-phillips.com>
- * @version 2.0.1 BETA (December 6th, 2009)
+ * @version 3.0 (October 2011)
  * @package Securimage
  *
  */
 
-include 'securimage.php';
+require_once dirname(__FILE__) . '/securimage.php';
 
-$img = new securimage();
+$img = new Securimage();
 
 //Change some settings
-$img->image_width = 280;
-$img->image_height = 100;
-$img->perturbation = 0.9;
-$img->code_length = rand(5,6);
-$img->image_bg_color = new Securimage_Color("#ffffff");
-$img->use_transparent_text = true;
-$img->text_transparency_percentage = 75; // 100 = completely transparent
-$img->num_lines = 15;
-$img->image_signature = '';
-$img->text_color = new Securimage_Color("#000000");
-$img->line_color = new Securimage_Color("#cccccc");
+$img->image_width     = 280;
+$img->image_height    = 100;
+$img->perturbation    = 0.9;      // high level of distortion
+$img->code_length     = rand(5,6); // random code length
+$img->image_bg_color  = new Securimage_Color("#ffffff");
+$img->num_lines       = 12;
+$img->noise_level     = 5;
+$img->text_color      = new Securimage_Color("#000000");
+$img->noise_color     = $img->text_color;
+$img->line_color      = new Securimage_Color("#cccccc");
 
-$img->show(''); // alternate use:  $img->show('/path/to/background_image.jpg');
-
+$img->show();
