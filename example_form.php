f274f14194de6125f62dced14b8587035ecc43e7
dapphp <drew@drew-phillips.com>
2012-05-30 09:56:35 -0700

    Fix css definition

--- example_form.php
+++ example_form.php
@@ -14,7 +14,7 @@ $GLOBALS['DEBUG_MODE'] = 1;
   <style type="text/css">
   <!--
   .error { color: #f00; font-weight: bold; font-size: 1.2em; }
-  .success { color: #00f; font-weight; bold; font-size: 1.2em; }
+  .success { color: #00f; font-weight: bold; font-size: 1.2em; }
   fieldset { width: 90%; }
   legend { font-size: 24px; }
   .note { font-size: 18px;
