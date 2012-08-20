be5de718de0048f6dacc272f494b8af6977d90d0
Drew <drew@downfall.(none)>
2012-08-19 22:26:08 -0700

    Increase max input length

--- example_form.php
+++ example_form.php
@@ -76,7 +76,7 @@ if (isset($_SESSION['ctform']['error']) &&  $_SESSION['ctform']['error'] == true
     <a tabindex="-1" style="border-style: none;" href="#" title="Refresh Image" onclick="document.getElementById('siimage').src = './securimage_show.php?sid=' + Math.random(); this.blur(); return false"><img src="./images/refresh.png" alt="Reload Image" height="32" width="32" onclick="this.blur()" align="bottom" border="0" /></a><br />
     <strong>Enter Code*:</strong><br />
      <?php echo @$_SESSION['ctform']['captcha_error'] ?>
-    <input type="text" name="ct_captcha" size="12" maxlength="8" />
+    <input type="text" name="ct_captcha" size="12" maxlength="16" />
   </p>
 
   <p>
