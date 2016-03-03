2f8b8a7952228a1e24a4dfc0ca8cb1aa4b83d603
dapphp <drew@drew-phillips.com>
2016-03-03 01:07:43 -0800

    Switch example code to use Securimage::getCaptchaHtml()

--- example_form.ajax.php
+++ example_form.ajax.php
@@ -24,6 +24,7 @@ process_si_contact_form();
         fieldset { width: 90%; }
         legend { font-size: 24px; }
         .note { font-size: 18px; }
+        form label { display: block; font-weight: bold; }
     -->
     </style>
     </head>
@@ -63,14 +64,7 @@ process_si_contact_form();
   </p>
 
   <p>
-    <img id="siimage" style="border: 1px solid #000; margin-right: 15px" src="./securimage_show.php?sid=<?php echo md5(uniqid()) ?>" alt="CAPTCHA Image" align="left" />
-    <object type="application/x-shockwave-flash" data="./securimage_play.swf?bgcol=#ffffff&amp;icon_file=./images/audio_icon.png&amp;audio_file=./securimage_play.php" height="32" width="32">
-    <param name="movie" value="./securimage_play.swf?bgcol=#ffffff&amp;icon_file=./images/audio_icon.png&amp;audio_file=./securimage_play.php" />
-    </object>
-    &nbsp;
-    <a tabindex="-1" style="border-style: none;" href="#" title="Refresh Image" onclick="document.getElementById('siimage').src = './securimage_show.php?sid=' + Math.random(); this.blur(); return false"><img src="./images/refresh.png" alt="Reload Image" height="32" width="32" onclick="this.blur()" align="bottom" border="0" /></a><br />
-    <strong>Enter Code*:</strong><br />
-    <input type="text" name="ct_captcha" size="12" maxlength="8" />
+    <?php require_once 'securimage.php'; echo Securimage::getCaptchaHtml(array('input_name' => 'ct_captcha')); ?>
   </p>
 
   <p>
@@ -92,21 +86,25 @@ process_si_contact_form();
 
     function processForm()
     {
-		jQuery.ajax({
-			url: '<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES) ?>',
-			type: 'POST',
-			data: jQuery('#contact_form').serialize(),
-			dataType: 'json',
-		}).done(function(data) {
-			if (data.error === 0) {
-				jQuery('#success_message').show();
-				jQuery('#contact_form')[0].reset();
-				reloadCaptcha();
-				setTimeout("jQuery('#success_message').fadeOut()", 12000);
-			} else {
-				alert("There was an error with your submission.\n\n" + data.message);
-			}
-		});
+        jQuery.ajax({
+            url: '<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES) ?>',
+            type: 'POST',
+            data: jQuery('#contact_form').serialize(),
+            dataType: 'json',
+        }).done(function(data) {
+            if (data.error === 0) {
+                jQuery('#success_message').show();
+                jQuery('#contact_form')[0].reset();
+                reloadCaptcha();
+                setTimeout("jQuery('#success_message').fadeOut()", 12000);
+            } else {
+                alert("There was an error with your submission.\n\n" + data.message);
+
+                if (data.message.indexOf('Incorrect security code') >= 0) {
+                    jQuery('#captcha_code').val('');
+                }
+            }
+        });
 
         return false;
     }
