26aadee5f6f40c1face9a67f5c9ee57ee7897234
dapphp <drew@drew-phillips.com>
2016-12-05 13:49:04 -0800

    CaptchaObject model

---
+++ CaptchaObject.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Securimage;
+
+class CaptchaObject
+{
+    public $captchaId;
+    public $captchaImageData;
+    public $captchaImageAudio;
+    public $creationTime;
+    public $code;
+    public $code_display;
+}
