40006f81d1b1f28a93cf03ee5ee1b699987061d7
Ondrej Esler <esler.ondrej@gmail.com>
2017-02-16 13:04:01 +0100

    add test for multiple namespaces

--- tests/SecureimageTest.php
+++ tests/SecureimageTest.php
@@ -23,6 +23,27 @@ class SecurimageTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($s1->getCode(), $s2->getCode());
     }
 
+    /**
+     * Tests session injection with multiple namespaces
+     *
+     * @return void
+     */
+    public function testInjectedSessionMultipleNamespaces() {
+        $GLOBALS['_SESSION'] = new ObjectSession;
+
+        $s1 = new Securimage(['namespace' => 'n1']);
+        $s1->createCode();
+        $s2 = new Securimage(['namespace' => 'n1']);
+        $this->assertEquals($s1->getCode(), $s2->getCode());
+
+        $s3 = new Securimage(['namespace' => 'n2']);
+        $s3->createCode();
+        $s4 = new Securimage(['namespace' => 'n2']);
+
+        $this->assertEquals($s1->getCode(), $s2->getCode()); // still working, which didn't after commit e1b0db4be0c247b0bb6303ab555868ad19ea1a1d
+        $this->assertEquals($s3->getCode(), $s4->getCode());
+    }
+
 }
 
 /**
