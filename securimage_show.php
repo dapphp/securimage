234c50aae3de79123f0badb5d4338d36d9faf7d6
Andreas Schnederle-Wagner <schnederle@futureweb.at>
2018-03-15 18:43:15 +0100

    Update securimage_show.php
    push Version to current

--- securimage_show.php
+++ securimage_show.php
@@ -39,7 +39,7 @@
  * @link http://www.phpcaptcha.org/Securimage_Docs/ Online Documentation
  * @copyright 2013 Drew Phillips
  * @author Drew Phillips <drew@drew-phillips.com>
- * @version 3.6.6 (Nov 20 2017)
+ * @version 3.6.7 (March 2018)
  * @package Securimage
  *
  */
