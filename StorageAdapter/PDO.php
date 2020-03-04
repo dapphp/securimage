8339b48cc01ef0f733cefc3ea21004a9580f95dd
Pieter Maes <maescool@gmail.com>
2020-03-05 00:11:43 +0100

    Fixes #93

--- StorageAdapter/PDO.php
+++ StorageAdapter/PDO.php
@@ -205,7 +205,7 @@ class PDO implements AdapterInterface
         if (!$result) {
             $err = $this->pdo_conn->errorInfo();
 
-            if ($this->database_driver == self::SI_DRIVER_SQLITE3 &&
+            if ($this->database_driver == \Securimage::SI_DRIVER_SQLITE3 &&
                 $err[1] === 1 && strpos($err[2], 'no such table') !== false)
             {
                 return false;
